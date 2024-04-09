fc5674db71df715be14b55923b64d5312754a364
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-04-09 13:19:01 -0400

    Pomodoro de cuenta regresiva funciona

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,28 +1,37 @@
-import { useEffect, useState } from "react"
-import { Container } from "./components/Container"
-import { Header } from "./components/Header"
+import React, { useEffect, useState } from 'react'
+import { Container } from './components/Container'
+import { Header } from './components/Header'
+import { usePodomoro } from './hooks/usePodomoro'
 import './assets/Button.css'
 import './assets/Popular.css'
 import './assets/Medio.css'
 import './assets/Largo.css'
 
 function App () {
+  const [isActive, setIsActive] = useState(false)
   const [seconds, setSeconds] = useState(0)
   const [minutes, setMinutes] = useState(0)
   const [hours, setHours] = useState(0)
-  const [descansos, setDescansos] = useState(0)
-  const [pomodoros, setPomodoros] = useState(0)
-  const [descansosLargos, setDescansosLargos] = useState(0)
-  const [isActive, setIsActive] = useState(false)
+  const [tipo, setTipo] = useState('pomodoro') // pomodoro - descanso - descansoLargo
+  const [ciclosCompletados, setCiclosCompletados] = useState(0)
+  const [descansosLargosTime, setDescansosLargosTime] = useState()
+  const [descansosTime, setDescansosTime] = useState()
+  const [pomodorosTime, setPomodorosTime] = useState()
+  const { Pomodoro } = usePodomoro({ descansosLargosTime, descansosTime, pomodorosTime })
 
-  //Recuperar objetos de elemento datos
+  // Recuperar objetos de elemento datos
   const handleSubmit = (event) => {
     event.preventDefault()
     const fields = Object.fromEntries(new window.FormData(event.target))
     console.log(fields)
   }
 
-  //Cuenta regresiva
+  // Reiniciar cronometro [Reset timer] -> TODO NO FUNCIONA [DO NOT WORKS]
+  const restart = () => {
+
+  }
+
+  // Cuenta regresiva
   useEffect(() => {
     let intervalID
 
@@ -32,9 +41,35 @@ function App () {
           setSeconds((seconds) => seconds - 1)
         } else {
           if (minutes === 0 && hours === 0) {
-            //audio
+            // audio
             clearInterval(intervalID)
             setIsActive(false)
+            // logica del pomodoro
+            if (tipo === 'pomodoro') {
+              if (ciclosCompletados < 3) {
+                setTipo('descanso')
+              } else {
+                setTipo('descansoLargo')
+                setCiclosCompletados(0)
+              }
+            } else if (tipo === 'descanso') {
+              setTipo('pomodoro')
+              setCiclosCompletados(prev => prev + 1)
+            } else if (tipo === 'descansoLargo') {
+              setTipo('pomodoro')
+              setIsActive(false)
+            }
+            // Reiniciar el temporizador al inicio de cada tipo
+            if (tipo === 'pomodoro') {
+              setMinutes(pomodorosTime)
+              setIsActive(true)
+            } else if (tipo === 'descanso') {
+              setMinutes(descansosTime)
+              setIsActive(true)
+            } else if (tipo === 'descansoLargo') {
+              setMinutes(descansosLargosTime)
+              setIsActive(true)
+            }
           } else {
             if (minutes === 0) {
               setHours((hours) => hours - 1)
@@ -50,78 +85,72 @@ function App () {
       clearInterval(intervalID)
     }
     return () => clearInterval(intervalID)
-  }, [isActive, hours, minutes, seconds, pomodoros])
+  }, [isActive, hours, minutes, seconds])
 
-  //Reiniciar cronometro [Reset timer] -> TODO NO FUNCIONA [DO NOT WORKS]
-  const restart = () => {
-    setIsActive(false)
-    setSeconds(0)
-    setMinutes(0)
-    setHours(0)
-  }
-
-  //Opciones predefinidas
+  // Opciones predefinidas
   const popular = () => {
-    setMinutes(20)
+    setDescansosLargosTime(3)
+    setDescansosTime(2)
+    setPomodorosTime(1)
     setIsActive(true)
   }
 
   return (
-    <>
+    <main>
       <Container>
-        <Header descansos={descansos} pomodoros={pomodoros} descansosLargos={descansosLargos} />
-        <section className="py-20">
-          <p className="text-center font-black text-8xl">
+        <Header descansos={1} pomodoros={1} descansosLargos={1} />
+        <section className='py-20'>
+          <p className='text-center font-black text-8xl'>
             {`
-            ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}
+            ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}
             `}
           </p>
           <div>
-            <button onClick={() => setIsActive(!isActive)} className="button">{isActive ? '⏸' : "▶"}</button>
+            <button onClick={() => setIsActive(!isActive)} className='button'>{isActive ? '⏸' : '▶'}</button>
 
-            <button onClick={() => restart} className="mx-2 p-2 border border-white border-solid rounded-lg" type="button">🔁</button>
+            <button onClick={() => restart} className='mx-2 p-2 border border-white border-solid rounded-lg' type='button'>🔁</button>
           </div>
         </section>
-        <section className="grid grid-cols-2 bottom-0">
-          <div className="flex flex-col max-w-52 gap-y-3">
-            <button className="Button">
-              <span className="Span">Popular</span>
-              <div className="top div"></div>
-              <div className="left div"></div>
-              <div className="bottom div"></div>
-              <div className="right div"></div>
+        <section className='grid grid-cols-2 bottom-0'>
+          <div className='flex flex-col max-w-52 gap-y-3'>
+            <button onClick={popular} className='Button'>
+              <span className='Span'>Popular</span>
+              <div className='top div' />
+              <div className='left div' />
+              <div className='bottom div' />
+              <div className='right div' />
             </button>
 
-            <button className="ButtonM">
-              <span className="SpanM">Medio</span>
-              <div className="topM divM"></div>
-              <div className="leftM divM"></div>
-              <div className="bottomM divM"></div>
-              <div className="rightM divM"></div>
+            <button className='ButtonM'>
+              <span className='SpanM'>Medio</span>
+              <div className='topM divM' />
+              <div className='leftM divM' />
+              <div className='bottomM divM' />
+              <div className='rightM divM' />
             </button>
 
-            <button className="ButtonL">
-              <span className="SpanL">Largo</span>
-              <div className="topL divL"></div>
-              <div className="leftL divL"></div>
-              <div className="bottomL divL"></div>
-              <div className="rightL divL"></div>
+            <button className='ButtonL'>
+              <span className='SpanL'>Largo</span>
+              <div className='topL divL' />
+              <div className='leftL divL' />
+              <div className='bottomL divL' />
+              <div className='rightL divL' />
             </button>
 
           </div>
-          <div className="flex flex-col items-center">
-            <p className="text-3xl font-bold">Personalizado</p>
-            <form onSubmit={handleSubmit} className="flex w-full flex-col max-w-sm">
-              <input min={0} max={120} type="range" name="Pomodoro" id="Pomodoro" />
-              <input min={0} max={60} type="range" name="Descanso" id="Descanso" />
-              <input min={0} max={60} type="range" name="DescansoLargo" id="DescansoLargo" />
-              <button className="mx-2 p-2 border border-white border-solid rounded-lg" type="submit">Comenzar</button>
+          <div className='flex flex-col items-center'>
+            <p className='text-3xl font-bold'>Personalizado</p>
+            <form onSubmit={handleSubmit} className='flex w-full flex-col max-w-sm'>
+              <input min={0} max={120} type='range' name='Pomodoro' id='Pomodoro' />
+              <input min={0} max={60} type='range' name='Descanso' id='Descanso' />
+              <input min={0} max={60} type='range' name='DescansoLargo' id='DescansoLargo' />
+              <button className='mx-2 p-2 border border-white border-solid rounded-lg' type='submit'>Comenzar</button>
             </form>
           </div>
         </section>
-      </Container >
+      </Container>
 
-    </>
+    </main>
   )
 }
 
